5da484714ab67115483575fa125b2a4676bf1f15
Hung Nguyen <hungnguyen.k51.ftu@gmail.com>
2024-02-08 23:44:22 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import DashboardPage from './pages/DashboardPage';
+
+function App() {
+  return (
+    <div className="App">
+      <DashboardPage></DashboardPage>
+    </div>
+  );
+}
+
+export default App;
